df0457a1651c74b4e4dcc1c3548b9cf9ea13737e
Kayleigh Stubbs <kaystubbs00@gmail.com>
2022-08-10 02:41:29 -0500

    Final commit

--- route/pages.js
+++ route/pages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Contact = require('./models/contact')
 
 router.get('/', (req, res)=>{
     res.render('index')
@@ -17,20 +18,8 @@ router.get('/cart', (req, res)=>{
     res.render('cart')
 })
 
-router.get('/favorites', (req, res)=>{
-    res.render('favorites')
-})
-
-router.get('/outerwear', (req, res)=>{
-    res.render('outerwear')
-})
-
-router.get('/sale', (req, res)=>{
-    res.render('sale')
-})
-
-router.get('/sets', (req, res)=>{
-    res.render('sets')
+router.get('/contact', (req, res)=>{
+    res.render('contact')
 })
 
 router.get('/shoes', (req, res)=>{
@@ -39,6 +28,22 @@ router.get('/shoes', (req, res)=>{
 
 router.get('/tops', (req, res)=>{
     res.render('tops')
+})
+
+router.post('/submitContact', (req, res) => {
+    const contact = new Contact ({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        reason: req.body.reason,
+        comment: req.body.comment
+    });
+
+    Contact.collection.insertOne(contact)
+    .then(result => {
+        res.render('contact')
+    })
+    .catch(err => console.log(err));
 })
 
 module.exports = router;
